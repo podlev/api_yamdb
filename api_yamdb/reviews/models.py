e29eca8601b2914d24188f63f14badf5ce47db7f
Сергеев Виталий <serg3evvitaly@yandex.ru>
2022-07-26 00:34:31 +0300

    Creating models again

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,16 +1,4 @@
-from django.contrib.auth import get_user_model
 from django.db import models
-
-User = get_user_model()
-
-
-class Users(models.Model):
-    pass
-
-
-class Titles(models.Model):
-    pass
-
 
 class Categories(models.Model):
     name = models.CharField(max_length=100)
@@ -20,9 +8,27 @@ class Categories(models.Model):
         return self.name
 
 
-class Genres(models.Model):
-    pass
+class Titles(models.Model):
+    name = models.CharField(max_length=50)
+    year = models.DateTimeField('Год создания')
+    category = models.ForeignKey(
+        Categories,
+        on_delete=models.CASCADE,
+        related_name='titles'
+    )
 
+    def __str__(self):
+        return self.name
+
+
+class Genres(models.Model):
+    name = models.CharField(max_length=50)
+    slug = models.SlugField()
+
+    def __str__(self):
+        return self.name
 
 class Genre_title(models.Model):
-    pass
+    title_id = models.
+
+
