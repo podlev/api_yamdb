e34df2addc600a5bec7e0b6e20ff5d0c06a27bf8
Зверева Ксения <7510325@mail.ru>
2022-08-01 16:24:22 +0300

    ksenia v mastere

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -1,4 +1,7 @@
-from reviews.models import Titles, Genre, Categories
+from django.shortcuts import get_object_or_404
+from rest_framework.exceptions import ValidationError
+
+from reviews.models import Titles, Genre, Categories, Review, Comments
 from rest_framework import serializers
 
 import datetime as dt
@@ -52,3 +55,56 @@ class TitlesSerializer(serializers.ModelSerializer):
                 raise serializers.ValidationError(
                     'Проверьте год издания произведения!')
             return value
+
+
+class ReviewSerializer(serializers.ModelSerializer):
+    title = serializers.SlugRelatedField(
+        slug_field='name',
+        read_only=True,
+    )
+    author = serializers.SlugRelatedField(
+        default=serializers.CurrentUserDefault(),
+        slug_field='username',
+        read_only=True
+    )
+    # class Meta:
+    #     model = Review
+    #     fields = ('author', 'title')
+    #     validators = [
+    #         serializers.UniqueTogetherValidator(
+    #             queryset=Review.objects.all(),
+    #             fields=['review', 'title'],
+    #             message='Невозможно добавить более '
+    #                     'одного отзыва на произведение'
+    #         )
+    #     ]
+
+    def validate(self, data):
+        request = self.context['request']
+        author = request.user
+        title_id = self.context['view'].kwargs.get('title_id')
+        title = get_object_or_404(Titles, pk=title_id)
+        if request.method == 'POST':
+            if Review.objects.filter(title=title, author=author).exists():
+                raise ValidationError('Вы не можете добавить более '
+                                      'одного отзыва на произведение')
+        return data
+
+    class Meta:
+        model = Review
+        fields = '__all__'
+
+
+class CommentsSerializer(serializers.ModelSerializer):
+    review = serializers.SlugRelatedField(
+        slug_field='text',
+        read_only=True
+    )
+    author = serializers.SlugRelatedField(
+        slug_field='username',
+        read_only=True
+    )
+
+    class Meta:
+        model = Comments
+        fields = '__all__'
--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,12 +1,15 @@
 from rest_framework.routers import DefaultRouter
 from django.urls import include, path
-from api.views import (CategoriesViewSet, GenreViewSet, TitlesViewSet)
+from api.views import (CategoriesViewSet, GenreViewSet,
+                       TitlesViewSet, CommentsViewSet, ReviewViewSet)
 
 router = DefaultRouter()
 
 router.register('categories', CategoriesViewSet, basename='categories')
 router.register('genres', GenreViewSet, basename='genres')
 router.register('titles', TitlesViewSet, basename='titles')
+router.register('comments', CommentsViewSet, basename='comments')
+router.register('review', ReviewViewSet, basename='review')
 
 urlpatterns = [
     path('v1/', include(router.urls)),
