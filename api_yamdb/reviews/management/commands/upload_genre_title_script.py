930dbd157fe17f782d83a8f272b9759ba14f27ad
Лев Подъельников <podlev@gmail.com>
2022-08-05 00:49:13 +0300

    Изменена логика регистрации пользователя (прошли все тесты)

--- api_yamdb/reviews/management/commands/upload_genre_title_script.py
+++ api_yamdb/reviews/management/commands/upload_genre_title_script.py
@@ -7,11 +7,11 @@ from reviews.models import GenreTitle, Title, Genre
 
 class Command(BaseCommand):
     def handle(self, **options):
-        with open("static/data/genre_title.csv",  encoding='utf-8') as csv_file:
+        with open("static/data/genre_title.csv", encoding='utf-8') as csv_file:
             reader = csv.DictReader(csv_file, delimiter=",")
             for row in reader:
                 GenreTitle.objects.create(
-                        id=row['id'],
-                        title_id=Title.objects.get(id=row['title_id']),
-                        genre_id=Genre.objects.get(id=row['genre_id'])
-                    )
+                    id=row['id'],
+                    title_id=Title.objects.get(id=row['title_id']),
+                    genre_id=Genre.objects.get(id=row['genre_id'])
+                )
